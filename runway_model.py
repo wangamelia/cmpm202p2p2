3f3d18c409ae7aeb598a000e4b7466564a853069
Peter Baylies <pbaylies@gmail.com>
2020-12-08 05:07:49 -0500

    Updates for runway model.

--- runway_model.py
+++ runway_model.py
@@ -20,15 +20,20 @@ def setup(opts):
 
 generate_inputs = {
     'z': runway.vector(512, sampling_std=0.5),
-    'truncation': runway.number(min=0, max=1, default=0.8, step=0.01)
+    'label': runway.number(min=0, max=100000, default=0, step=1)
+    'scale': runway.number(min=-5, max=5, default=0, step=0.05)
+    'truncation': runway.number(min=0, max=1, default=1, step=0.01)
 }
 
 @runway.command('generate', inputs=generate_inputs, outputs={'image': runway.image})
 def convert(model, inputs):
     z = inputs['z']
+    label = int(inputs['label'])
+    scale = inputs['scale']
     truncation = inputs['truncation']
     latents = z.reshape((1, 512))
-    images = model.run(latents, None, truncation_psi=truncation, randomize_noise=False, output_transform=fmt)
+    labels = scale * np.random.RandomState(label).randn(167)
+    images = model.run(latents, labels, truncation_psi=truncation, randomize_noise=False, output_transform=fmt)
     output = np.clip(images[0], 0, 255).astype(np.uint8)
     return {'image': output}
 
